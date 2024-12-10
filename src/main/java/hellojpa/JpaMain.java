07dd64bd5839405e80f0d71ad18ab1e985884153
kwonjiyoon <yoon980112@naver.com>
2024-12-10 22:10:14 +0900

    feat: chapter07, 08 - JPA 시작하기

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,15 +2,41 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
 
+        // 애플리케이션 로딩 시 한번만 생성하여 애플리케이션 전체에서 공유
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        // 스레드간 공유 X :: 사용하고 버려야 함
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        // JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            // Member findMember = em.find(Member.class, 1L);
+            // findMember.setName("HelloJPA");
+
+            // JPQL :: 테이블이 아닌 객체를 대상으로
+            // => 객체 지향 SQL
+            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
