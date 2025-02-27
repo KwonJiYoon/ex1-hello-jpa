bebaecb24a951e0c04acda311bf9e763bbebdcd2
kwonjiyoon <yoon980112@naver.com>
2025-02-27 21:55:33 +0900

    feat: chapter16 - 필드와 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,16 +19,12 @@ public class JpaMain {
 
         try {
 
-            // 영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAA");
+            Member member = new Member();
+            member.setId(4L);
+            member.setName("C");
+            member.setRoleType(RoleType.GUEST);
 
-            // em.detach(member);
-            em.clear();
-
-            Member member2 = em.find(Member.class, 150L);
-
-            System.out.println("=================");
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,31 +1,69 @@
 package hellojpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
 public class Member {
 
     @Id
     private Long id;
-    private String name;
+
+    // updatable = false : 업데이트 되지 않음
+    // nullable = false : not null
+    // unique = true : 유니크 제약 조건을 만들어주긴하나 이름을 랜덤으로 생성하여 잘 사용하지 않음
+    // columnDefinition = "varchar(100) default 'EMPTY' : 데이터베이스 컬럼 정보 직접 줄 수 있음
+    @Column(name = "name", updatable = false)
+    private String userName;
+
+    private Integer age;
+
+    // EnumType.ORNiDAL : enum의 순서 저장
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    @Transient  // DB 반영 X
+    private int temp;
 
     public Member() {
     }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
 
     public Long getId() {
         return id;
     }
 
     public String getName() {
-        return name;
+        return userName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public void setId(Long id) {
@@ -33,6 +71,26 @@ public class Member {
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.userName = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
